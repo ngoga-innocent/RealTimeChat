32ab5304264192673cec6611b17b7a4b9d654471
Ngoga <ngogainnocent1@gmail.com>
2023-09-07 11:28:00 +0200

    ==>urls

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,67 +1,87 @@
+import React, { useState } from "react";
+import Cookies from "universal-cookie";
+import { ClipLoader } from "react-spinners";
+const Login = (props) => {
+  const cookies = new Cookies();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // const [name, setName] = useState("");
+  const [logged, setIsLogged] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const Login = async (e) => {
+    setIsLoading(true);
+    e.preventDefault();
+    if (username !== "" && password !== "") {
+      var formdata = new FormData();
+      formdata.append("username", username);
+      formdata.append("password", password);
 
-import React,{useState} from "react";
-import Cookies from "universal-cookie"
+      var requestOptions = {
+        method: "POST",
+        // headers: myHeaders,
+        body: formdata,
+        redirect: "follow",
+      };
 
-const Login =(props)=>{
-   const cookies=new Cookies()
-  const [username,setUsername]=useState('')
-  const [password,setPassword]=useState('')
-  const [name,setName]=useState('')
-  const [logged,setIsLogged]=useState(false)
-  const Login =(e)=>{
-    e.preventDefault()
-    if(username !== ''&& password !==''){
-        var formdata = new FormData();
-formdata.append("username", username);
-formdata.append("password", password);
+      fetch("https://www.kaznikaz.com/api/chat/login", requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+          console.log(result.token.access, result.user_info);
+          console.log(result);
+          if (result.token) {
+            //setName(result.user_info.username);
+            setIsLogged(true);
+            props.getName(result.user_info.username, true);
+            cookies.set("token", result.token.access);
+            window.location.replace("/");
+            setIsLoading(false);
+          } else {
+            console.log("invalid credentials");
+          }
+        })
+        .catch((error) => console.log("error", error));
+ 
+    } else {
+      console.log("no password or emaill");
+    }
+  };
 
-var requestOptions = {
-  method: 'POST',
-  body: formdata,
-  redirect: 'follow'
+  return (
+    <div className="flex flex-col h-screen items-center justify-center bg-gradient-to-b from-teal-300 to-teal-950">
+      <ClipLoader
+        loading={isLoading}
+        color="red"
+        size={150}
+        arial-label="loading Spiner"
+        data-testid="loader"
+      />
+      <div className=" h-1/2 w-1/3 flex flex-col  bg-slate-500 shadow-lg shadow-white items-center justify-center rounded-lg">
+        <h1 className="text-white font-bold text-3xl">Login</h1>
+        <form className="p-5 flex flex-col space-y-8 w-full" onSubmit={Login}>
+          <input
+            type="text"
+            placeholder="username or Email"
+            className="bg-transparent  w-full border-b-white border-2 py-2 rounded-md px-3 text-white font-semibold"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          {/* <input type="email" placeholder="email" className="bg-transparent  w-full border-b-white border-2 py-2 rounded-md px-3  text-white font-semibold" /> */}
+          <input
+            type="password"
+            placeholder="password"
+            className="bg-transparent  w-full border-b-white border-2 py-2 rounded-md px-3  text-white font-semibold"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" className=" rounded-md bg-white py-3 font-bold">
+            Login
+          </button>
+        </form>
+        <span>
+          <a href="/register">Register here</a>
+        </span>
+      </div>
+    </div>
+  );
 };
-
-
-fetch("http://localhost:8000/chat/login", requestOptions)
-  .then(response => response.json())
-  .then(result => {
-     console.log(result.token.access,result.user_info)
-    if(result.token){
-        // setName(result.user_info.username)
-        setIsLogged(true)
-        props.getName(result.user_info.username,true)
-        cookies.set("token",result.token.access)
-        window.location.replace('/')
-        
-    }
-    else{
-        console.log("invalid credentials")
-    }
-
-})
-  .catch(error => console.log('error', error));
-    }
-    else{
-        console.log('no password or emaill')
-    }
-  }
-
-    return(
-        <div className="flex flex-col h-screen items-center justify-center bg-gradient-to-b from-teal-300 to-teal-950">
-           
-           <div className=" h-1/2 w-1/3 flex flex-col  bg-slate-500 shadow-lg shadow-white items-center justify-center rounded-lg">
-           <h1 className="text-white font-bold text-3xl">Login</h1>
-           <form className="p-5 flex flex-col space-y-8 w-full" onSubmit={Login}>
-            <input type="text" placeholder="username or Email" className="bg-transparent  w-full border-b-white border-2 py-2 rounded-md px-3 text-white font-semibold"  value={username} onChange={(e)=>setUsername(e.target.value)} />
-            {/* <input type="email" placeholder="email" className="bg-transparent  w-full border-b-white border-2 py-2 rounded-md px-3  text-white font-semibold" /> */}
-            <input type="password" placeholder="password" className="bg-transparent  w-full border-b-white border-2 py-2 rounded-md px-3  text-white font-semibold" value={password} onChange={(e)=>setPassword(e.target.value)} />
-            <button className=" rounded-md bg-white py-3 font-bold">Login</button>
-           </form>
-           <span>
-            <a href="/register">Register here</a>
-           </span>
-           </div>
-        </div>
-    )
-}
-export default Login
+export default Login;
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,53 +1,87 @@
-
-import React,{useState} from "react";
-
-const Register =()=>{
-    const [username,setUsername]=useState('')
-    const [email,setEmail]=useState('')
-    const [password,setPassword]=useState('')
-    
-const RegisterUser=(e)=>{
-    e.preventDefault()
+import React, { useState } from "react";
+import { ClipLoader } from "react-spinners";
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const RegisterUser = (e) => {
+    setIsLoading(true)
+    e.preventDefault();
     var formdata = new FormData();
     formdata.append("username", username);
     formdata.append("password", password);
     formdata.append("email", email);
-    
-    var requestOptions = {
-      method: 'POST',
-      body: formdata,
-      redirect: 'follow'
-    };
-    
-    fetch("http://localhost:8000/chat/register", requestOptions)
-      .then(response => response.json())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
-}
 
-    return(
-        <div className="flex flex-col h-screen items-center justify-center bg-gradient-to-b from-teal-300 to-teal-950">
-           
-           <div className=" h-1/2 w-1/3 flex flex-col  bg-slate-500 shadow-lg shadow-white items-center justify-center rounded-lg">
-           <h1 className="text-white font-bold text-3xl">Register</h1>
-           <form className="p-5 flex flex-col space-y-8 w-full" onSubmit={RegisterUser}>
-            
-            <input value={username} onChange={(e)=>setUsername(e.target.value)} type="text" placeholder="username" 
-            className="bg-transparent  w-full border-b-white border-2 py-2 rounded-md px-3 text-white font-semibold" />
-            
-            <input type="email" placeholder="email"value={email} onChange={(e)=>setEmail(e.target.value)} 
-            className="bg-transparent  w-full border-b-white border-2 py-2 rounded-md px-3  text-white font-semibold" />
-            
-            <input 
-            type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="password" className="bg-transparent  w-full border-b-white border-2 py-2 rounded-md px-3  text-white font-semibold" />
-            
-            <button type='submit' className=" rounded-md bg-white py-3 font-bold">Register</button>
-           </form>
-           <span>
-            <a href="/login">Login here</a>
-           </span>
-           </div>
-        </div>
-    )
-}
-export default Register
+    var requestOptions = {
+      method: "POST",
+      body: formdata,
+      redirect: "follow",
+    };
+
+    fetch("https://www.kaznikaz.com/api/chat/register", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        if(result.user_info){
+            window.location.replace("login");
+        }
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log("error", error);
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <div className="flex flex-col h-screen items-center justify-center bg-gradient-to-b from-teal-300 to-teal-950">
+      <ClipLoader
+        loading={isLoading}
+        color="red"
+        size={150}
+        arial-label="loading Spiner"
+        data-testid="loader"
+      />
+      <div className=" h-1/2 w-1/3 flex flex-col  bg-slate-500 shadow-lg shadow-white items-center justify-center rounded-lg">
+        <h1 className="text-white font-bold text-3xl">Register</h1>
+        <form
+          className="p-5 flex flex-col space-y-8 w-full"
+          onSubmit={RegisterUser}
+        >
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            type="text"
+            placeholder="username"
+            className="bg-transparent  w-full border-b-white border-2 py-2 rounded-md px-3 text-white font-semibold"
+          />
+
+          <input
+            type="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="bg-transparent  w-full border-b-white border-2 py-2 rounded-md px-3  text-white font-semibold"
+          />
+
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="password"
+            className="bg-transparent  w-full border-b-white border-2 py-2 rounded-md px-3  text-white font-semibold"
+          />
+
+          <button type="submit" className=" rounded-md bg-white py-3 font-bold">
+            Register
+          </button>
+        </form>
+        <span>
+          <a href="/login">Login here</a>
+        </span>
+      </div>
+    </div>
+  );
+};
+export default Register;
